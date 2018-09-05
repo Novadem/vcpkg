e69bcf519b71728f3ec9da8152096672ce6f36d8
Tom Henoch <tom.henoch@novadem.com>
2018-09-05 17:16:56 +0200

    Fix portfile

--- ports/corvusoft-core/portfile.cmake
+++ ports/corvusoft-core/portfile.cmake
@@ -18,6 +18,18 @@ vcpkg_install_cmake()
 #Remove include debug files
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
+# Move and rename library
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/lib)
+file(COPY ${CURRENT_PACKAGES_DIR}/library/core.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+file(RENAME ${CURRENT_PACKAGES_DIR}/lib/core.lib ${CURRENT_PACKAGES_DIR}/lib/corvusoft-core.lib)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/library)
+
+# Do the same for debug build
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/lib)
+file(COPY ${CURRENT_PACKAGES_DIR}/debug/library/core.lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
+file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/core.lib ${CURRENT_PACKAGES_DIR}/debug/lib/corvusoft-core.lib)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/library)
+
 # Handle copyright
 file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/corvusoft-core)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/corvusoft-core/LICENSE ${CURRENT_PACKAGES_DIR}/share/corvusoft-core/copyright)
